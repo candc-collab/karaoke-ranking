9ba7f0b77e33db131920f653e0f64a00c91f3223
candc-collab <kakimoto.candc@gmail.com>
2026-08-29 17:06:32 +0900

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 window.KARAOKE_APP_CONFIG = {
-  LIFF_ID: 'YOUR_LIFF_ID',
-  GAS_API_URL: 'YOUR_GAS_WEB_APP_EXEC_URL'
+  LIFF_ID: '2011318355-6xHiyDqx',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbz1J4yMghmV4Rbtxg4usSmsRRu-0kdnKk5n-pZrWy4UD8hQ02eooNhZbesOjzK5T_PMnQ/exec'
 };
